97e1db3236ae76a8aafb042beb1e5642c0f1cc82
Gui-dev <adrianon10@gmail.com>
2026-05-09 05:19:29 -0300

    feat(ui): add appointment card component

---
+++ src/components/appointment-card.tsx
@@ -0,0 +1,27 @@
+import type { AppointmentProps } from '@/types/appointment'
+
+interface IAppointmentCardProps {
+  appointment: AppointmentProps
+}
+
+export const AppointmentCard = ({ appointment }: IAppointmentCardProps) => {
+  return (
+    <div className="grid grid-cols-2 items-center border-zinc-800 border-b py-3 [ last:border-b-0 md:grid-cols-[15%_35%_30%_20%]">
+      <div className="pr-4 text-left md:pr-0">
+        <span className="font-semibold text-sm text-zinc-100">{appointment.time}</span>
+      </div>
+
+      <div className="text-right md:pr-4 md:text-left">
+        <div className="flex items-center justify-end gap-1 md:justify-start">
+          <span className="font-semibold text-sm text-zinc-100">{appointment.petName}</span>
+          <span className="text-sm text-zinc-400">/</span>
+          <span className="font-semibold text-sm text-zinc-100">{appointment.tutorName}</span>
+        </div>
+
+      </div>
+      <div className="col-span-2 mt-1 hidden gap-2 pr-4 text-left md:col-span-1 md:mt-0 md:block">
+        <span className="text-sm text-zinc-400">{appointment.description}</span>
+      </div>
+    </div>
+  )
+}
