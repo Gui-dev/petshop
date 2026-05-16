7a49992530e2a39d02ca5aafe7bf0c9cb575e087
Gui-dev <adrianon10@gmail.com>
2026-05-16 05:40:19 -0300

    feat(appointment): add date and time scheduling to form
    Add date picker, time slot selection, loading state, and toast
    notifications. Update schema with scheduledAt and time validation.

--- src/components/appointment-form.tsx
+++ src/components/appointment-form.tsx
@@ -1,14 +1,18 @@
 'use client'
 
 import { standardSchemaResolver } from '@hookform/resolvers/standard-schema'
-import { Calendar1, Dog, Phone, User } from 'lucide-react'
-import { useForm } from 'react-hook-form'
+import { format, startOfToday } from 'date-fns'
+import { Calendar, Calendar1, ChevronDown, Clock, Dog, Loader, Phone, User } from 'lucide-react'
+import { Controller, useForm } from 'react-hook-form'
 import { IMaskInput } from 'react-imask'
+import { toast } from 'sonner'
 import {
   appointmentFormSchema,
   type IAppointmentFormSchema,
 } from '@/schemas/appointment-form-schema'
+import { timeOptions } from '@/utils/generate-time-options'
 import { Button } from './ui/button'
+import { Calendar as CalendarComponent } from './ui/calendar'
 import {
   Dialog,
   DialogContent,
@@ -18,13 +22,16 @@ import {
   DialogTrigger,
 } from './ui/dialog'
 import { Input } from './ui/input'
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select'
 import { Textarea } from './ui/textarea'
 
 export const AppointmentForm = () => {
   const {
+    control,
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<IAppointmentFormSchema>({
     resolver: standardSchemaResolver(appointmentFormSchema),
     defaultValues: {
@@ -32,11 +39,16 @@ export const AppointmentForm = () => {
       petName: '',
       phone: '',
       description: '',
+      scheduledAt: new Date(),
+      time: '',
     },
   })
 
   const handleSubmitAppointment = (data: IAppointmentFormSchema) => {
-    console.log(data)
+    const [hour, minute] = data.time.split(':')
+    const scheduledDate = new Date(data.scheduledAt).setHours(Number(hour), Number(minute), 0, 0)
+    toast.success('Agendamento realizado com sucesso!')
+    console.log(scheduledDate)
   }
 
   return (
@@ -48,7 +60,7 @@ export const AppointmentForm = () => {
         </Button>
       </DialogTrigger>
       <DialogContent>
-        <div className="flex flex-col items-center justify-center gap-4">
+        <div className="flex flex-col gap-4">
           <DialogHeader>
             <DialogTitle>Agende um atendimento</DialogTitle>
             <DialogDescription>
@@ -98,15 +110,22 @@ export const AppointmentForm = () => {
             <div className="flex flex-col gap-2">
               <label
                 htmlFor="phone"
-                className="flex cursor-pointer items-center gap-1 rounded-lg bg-zinc-700 px-4 py-2 focus-within:ring-2 focus-within:ring-yellow-400"
+                className="flex cursor-pointer items-center gap-3 rounded-lg bg-zinc-700 px-4 py-2 focus-within:ring-2 focus-within:ring-yellow-400"
               >
                 <Phone className="size-4" />
-                <IMaskInput
-                  mask="(00) 00000-0000"
-                  id="phone"
-                  {...register('phone')}
-                  placeholder="(99) 99999-9999"
-                  className="bg-transparent py-2 outline-none"
+                <Controller
+                  control={control}
+                  name="phone"
+                  render={({ field }) => (
+                    <IMaskInput
+                      mask="(00) 00000-0000"
+                      id="phone"
+                      {...field}
+                      type="tel"
+                      placeholder="(99) 99999-9999"
+                      className="bg-transparent py-2 outline-none"
+                    />
+                  )}
                 />
               </label>
               {errors.phone && (
@@ -131,8 +150,82 @@ export const AppointmentForm = () => {
               )}
             </div>
 
-            <Button type="submit" className="h-12 rounded-lg">
-              Agendar
+            <div className="flex flex-col gap-4 md:mb-4 md:flex-row md:items-center md:gap-2">
+              <div className="flex flex-col gap-2 md:w-full">
+                <Controller
+                  control={control}
+                  name="scheduledAt"
+                  render={({ field }) => (
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <button
+                          type="button"
+                          className="flex cursor-pointer items-center justify-between gap-1 rounded-lg bg-zinc-700 py-4 pr-4 pl-2 focus-within:ring-2 focus-within:ring-yellow-400"
+                        >
+                          <span className="flex items-center gap-2">
+                            <Calendar className="size-4" />
+                            {field.value && format(field.value, 'dd/MM/yyyy')}
+                            {!field.value && <span>Data</span>}
+                          </span>
+                          <ChevronDown className="size-4 font-bold" />
+                        </button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <CalendarComponent
+                          mode="single"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          disabled={date => date < startOfToday()}
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  )}
+                />
+                {errors.scheduledAt && (
+                  <span className="text-left text-red-500 text-xs">
+                    {errors.scheduledAt.message}
+                  </span>
+                )}
+              </div>
+
+              <div className="flex flex-col gap-2 md:w-full">
+                <Controller
+                  control={control}
+                  name="time"
+                  render={({ field }) => (
+                    <label
+                      htmlFor="time"
+                      className="flex items-center rounded-lg bg-zinc-700 py-2 focus-within:ring-2 focus-within:ring-yellow-400"
+                    >
+                      <Select value={field.value} onValueChange={field.onChange}>
+                        <SelectTrigger id="time" className="w-full bg-transparent">
+                          <div className="flex items-center gap-2">
+                            <Clock className="size-4 text-zinc-100" />
+                            <SelectValue placeholder="Hora" />
+                          </div>
+                        </SelectTrigger>
+                        <SelectContent>
+                          {timeOptions.map(option => {
+                            return (
+                              <SelectItem key={option.value} value={option.value}>
+                                {option.label}
+                              </SelectItem>
+                            )
+                          })}
+                        </SelectContent>
+                      </Select>
+                    </label>
+                  )}
+                />
+                {errors.time && (
+                  <span className="text-left text-red-500 text-xs">{errors.time.message}</span>
+                )}
+              </div>
+            </div>
+
+            <Button type="submit" className="h-12 rounded-lg font-bold" disabled={isSubmitting}>
+              {!isSubmitting && 'Agendar'}
+              {isSubmitting && <Loader className="size-6 animate-spin text-zinc-800" />}
             </Button>
           </form>
         </div>
--- src/schemas/appointment-form-schema.ts
+++ src/schemas/appointment-form-schema.ts
@@ -1,12 +1,28 @@
+import { setHours, setMinutes, startOfToday } from 'date-fns'
 import { z } from 'zod'
 
-export const appointmentFormSchema = z.object({
-  tutorName: z.string().min(1, 'O nome do tutor é obrigatório'),
-  petName: z.string().min(1, 'O nome do pet é obrigatório'),
-  phone: z
-    .string('O telefone é obrigatório')
-    .min(11, 'O número de telefone deve ter pelo menos 11 dígitos'),
-  description: z.string().min(3, 'A descrição é obrigatória'),
-})
+export const appointmentFormSchema = z
+  .object({
+    tutorName: z.string().min(1, 'O nome do tutor é obrigatório'),
+    petName: z.string().min(1, 'O nome do pet é obrigatório'),
+    phone: z.string().min(11, 'O número de telefone deve ter pelo menos 11 dígitos'),
+    description: z.string().min(3, 'A descrição é obrigatória'),
+    scheduledAt: z
+      .date({ error: 'A data é obrigatória' })
+      .min(startOfToday(), 'A data deve ser futura'),
+    time: z.string().min(1, 'O horário é obrigatório'),
+  })
+  .refine(
+    data => {
+      const [hour, minute] = data.time.split(':')
+      const scheduleDateTime = setMinutes(setHours(data.scheduledAt, Number(hour)), Number(minute))
+
+      return scheduleDateTime > new Date()
+    },
+    {
+      path: ['time'],
+      message: 'O horário deve ser futuro',
+    }
+  )
 
 export type IAppointmentFormSchema = z.infer<typeof appointmentFormSchema>
