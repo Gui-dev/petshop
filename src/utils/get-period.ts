02435fd1347047c760c265660daae72e0a01b656
Gui-dev <adrianon10@gmail.com>
2026-05-20 04:13:26 -0300

    fix(utils): adjust getPeriod time ranges for business hours

--- src/utils/get-period.ts
+++ src/utils/get-period.ts
@@ -1,11 +1,15 @@
 import type { PeriodType } from '@/types/appointment'
 
 export const getPeriod = (hour: number): PeriodType => {
-  if (hour >= 0 && hour < 12) {
-    return 'morning'
-  } else if (hour >= 12 && hour < 18) {
-    return 'afternoon'
-  } else {
-    return 'evening'
+  let type: PeriodType = 'morning'
+
+  if (hour >= 9 && hour < 12) {
+    type = 'morning'
+  } else if (hour >= 13 && hour < 18) {
+    type = 'afternoon'
+  } else if (hour >= 19 && hour < 21) {
+    type = 'evening'
   }
+
+  return type
 }
